7377d30ab67dfb29c6123f1ad5e085bb16a5e624
Javier Andres Olivera Soto <javierandresoliverasoto@gmail.com>
2022-07-23 13:17:26 -0500

    inicial para hacer el programa correr con lista que sea dinamica y no por medio de String

---
+++ ExpresionPilayLista.java
@@ -0,0 +1,181 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package expresionpilaylista;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author beraxes
+ */
+public class ExpresionPilayLista {
+
+    nodo cima;
+    nodo falda;
+    nodo cabeza;
+    nodo fin;
+
+    public ExpresionPilayLista() {
+        cima = null;
+        falda = null;
+        cabeza = null;
+        fin = null;
+    }
+
+    public void listarYEnlistar(String caracter) {
+        if (cabeza == null) //no hay lista
+        {
+            cabeza = new nodo();
+            cabeza.setDato(caracter);
+            fin = cabeza;
+        } else {
+            nodo temporal = new nodo();
+            temporal.setDato(caracter);
+            fin.setEnlace(temporal);
+            fin = temporal;
+        }
+
+    }
+
+    public void apilar(String item) {
+        if (falda == null) {
+            falda = new nodo();
+            falda.setDato(item);
+            cima = falda;
+
+        } else {
+            nodo temp = new nodo();
+            temp.setDato(item);
+            temp.setEnlace(cima);
+            cima = temp;
+        }
+    }
+
+    public void desapilarTodo() {
+        nodo temp;
+
+        if (cima != null) {
+            while (cima != null) {                
+
+                if (cima == falda) {
+                    listarYEnlistar(cima.getDato());
+                    cima = null;
+                    falda = null;
+
+                } else {
+                    temp = cima;
+                    listarYEnlistar(cima.getDato());
+
+                    cima = cima.getEnlace();
+                    temp.setEnlace(null);
+                }
+
+            }
+        }
+
+    }
+
+    public String desapilarElemento() {
+        nodo temp;
+        String aux = "";
+        if (cima != null) {
+
+            // cima.mostrar();
+            if (cima == falda) {
+                aux = aux + cima.getDato();
+                cima = null;
+                falda = null;
+
+            } else {
+                temp = cima;
+                aux = cima.getDato();
+                cima = cima.getEnlace();
+                temp.setEnlace(null);
+            }
+
+        }
+        return aux;
+    }
+
+    public static int Priorizar(String signo) {
+        int valorPrioridad = -1;
+        if (signo.equals("+") || signo.equals("-")) {
+            valorPrioridad = 1;
+        } else if (signo.equals("*") || signo.equals("/")) {
+            valorPrioridad = 2;
+        } else if (signo.equals("^")) {
+            valorPrioridad = 3;
+        }
+        return valorPrioridad;
+    }
+
+    public static void main(String[] args) {
+        String expresionUsuario = JOptionPane.showInputDialog("Ingrese una expresion"); // aqui pido por joption un string al usuario que seria una expresion matematica
+        ExpresionPilayLista pila = new ExpresionPilayLista();
+        for (int i = 0; i < expresionUsuario.length(); i++) {
+            String aux = "";
+            aux = aux + expresionUsuario.charAt(i);
+            if (aux.equals("(")) {
+                pila.apilar(aux);
+
+            } else if (aux.equals(")")) {
+                String dato;
+                do {
+                    dato = pila.desapilarElemento();
+                    if (!dato.equals("(")) {
+                        pila.listarYEnlistar(dato);
+                    }
+                } while (!dato.equals("("));
+
+            } else if (aux.equals("+") || aux.equals("-") || aux.equals("*") || aux.equals("/") || aux.equals("^")) {
+                int operador = Priorizar(aux);
+
+                do {
+                    if (pila.cima == null) {
+                        pila.apilar(aux);
+                        aux = "";
+                    } else {
+
+                        String salientePila = pila.desapilarElemento();
+                        if (salientePila.equals("(")) {
+                            pila.apilar(salientePila);
+                            pila.apilar(aux);
+                            aux = "";
+                        } else {
+                            int precedencia = Priorizar(salientePila);
+                            if (operador == precedencia) {
+                                pila.listarYEnlistar(salientePila);
+
+                            } else if (operador > precedencia) {
+                                pila.apilar(salientePila);
+                                pila.apilar(aux);
+                                aux = "";
+                            } else {
+                                pila.listarYEnlistar(salientePila);
+                                aux = "";
+
+                            }
+                        }
+                    }
+
+                } while (!aux.equals(""));
+            } else {
+                pila.listarYEnlistar(aux);
+
+            }
+
+        }
+
+        String resto = "";
+        if (pila.cima != null) {
+            resto = pila.desapilarTodo();
+        }
+
+        epos = epos + pila.vacioComilla(epos, resto);
+
+        System.out.println(epos);
+
+    }
+}
---
+++ nodo.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package expresionpilaylista;
+
+/**
+ *
+ * @author beraxes
+ */
+public class nodo {
+
+    private String Dato;
+    public nodo enlace;
+
+    public nodo() {
+        Dato = "";
+        enlace = null;
+    }
+
+    public String getDato() {
+        return Dato;
+    }
+
+    public void setDato(String Dato) {
+        this.Dato = Dato;
+    }
+
+    public nodo getEnlace() {
+        return enlace;
+    }
+
+    public void setEnlace(nodo enlace) {
+        this.enlace = enlace;
+    }
+
+    public void mostrar() {
+        System.out.println(getDato());
+    }
+
+}
